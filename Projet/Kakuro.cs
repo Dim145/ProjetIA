96e81148bc38e04a4cdc38bf360292d9a496dc40
Dim145 <dimitri.bleach@gmail.com>
2022-05-11 10:00:41 +0200

    init avec exemple cours

---
+++ Projet/Kakuro.cs
@@ -0,0 +1,77 @@
+﻿namespace Projet;
+
+public class Kakuro
+{
+    private int?[,] TabValues { get; }
+    private int?[,][] TabIndices { get; }
+    
+    public int NbCol { get; }
+    public int NbLig { get; }
+
+    public Kakuro(int nbLig, int nbCol)
+    {
+        TabIndices = new int?[nbLig, nbCol][];
+        TabValues = new int?[nbLig, nbCol];
+
+        NbCol = nbCol;
+        NbLig = nbLig;
+    }
+
+    public void Initialize(int?[,][]? tabIndices = null, int?[,]? tabValues = null )
+    {
+        if (tabIndices is null || tabValues is null || tabIndices.Length != TabIndices.Length || tabValues.Length != TabValues.Length)
+        {
+            return;
+        }
+
+        for (int i = 0; i < NbLig; i++)
+        {
+            for (int j = 0; j < NbCol; j++)
+            {
+                if (tabIndices[i, j] == null)
+                    TabValues[i, j] = tabValues[i, j];
+                else
+                    TabIndices[i, j] = tabIndices[i, j];
+            }
+        }
+    }
+
+    public int? GetValue(int lig, int col)
+    {
+        return TabValues[lig, col];
+    }
+
+    public int?[] GetIndices(int lig, int col)
+    {
+        return TabIndices[lig, col];
+    }
+
+    public object Get(int lig, int col)
+    {
+        return GetValue(lig, col) as object ?? GetIndices(lig, col);
+    }
+
+    public override string ToString()
+    {
+        var sRet = string.Empty;
+
+        for (int i = 0; i < NbLig; i++)
+        {
+            for (int j = 0; j < NbCol; j++)
+            {
+                var tabCase = Get(i, j);
+                
+                sRet += tabCase switch
+                {
+                    int val => $"|  {val}  |",
+                    int?[] indices => $"|{(indices[0] is null ? " □" : $"{indices[0]:D2}")}\\{(indices[1] is null ? "□ " : $"{indices[1]:D2}")}|",
+                    _ => "|  □  |"
+                };
+            }
+
+            sRet += "\n";
+        }
+
+        return sRet;
+    }
+}
--- Projet/Program.cs
+++ Projet/Program.cs
@@ -1,1 +1,31 @@
-﻿var i = 0;
+﻿using Projet;
+
+var kakuro = new Kakuro(8, 8);
+
+var tabIndices = new[,]
+{
+    {null, new int?[]{23, null}, new int?[]{30, null}, null, null, new int?[]{27, null}, new int?[]{12, null}, new int?[]{16, null} },
+    {new int?[]{null, 16}, null, null, null, new int?[]{17, 24}, null, null, null },
+    {new int?[]{null, 17}, null, null, new int?[]{15, 29}, null, null, null, null },
+    {new int?[]{null, 35}, null, null, null, null, null, new int?[]{12, null}, null },
+    {null, new int?[]{null, 7}, null, null, new int?[]{7, 8}, null, null, new int?[]{7, null} },
+    {null, new int?[]{11, null}, new int?[]{10, 16}, null, null, null, null, null },
+    {new int?[]{null, 21}, null, null, null, null, new int?[]{null, 5}, null, null },
+    {new int?[]{null, 6}, null, null, null, null, new int?[]{null, 3}, null, null }
+};
+
+var tabValue = new int?[,]
+{
+    {null, null, null, null, null, null, null, null},
+    {null, 0, 0, null, null, 0, 0, 0},
+    {null, 0, 0, null, 0, 0, 0, 0},
+    {null, 0, 0, 0, 0, 0, null, null},
+    {null, null, 0, 0, null, 0, 0, null},
+    {null, null, null, 0, 0, 0, 0, 0},
+    {null, 0, 0, 0, 0, null, 0, 0},
+    {null, 0, 0, 0, null, null, 0, 0}
+};
+
+kakuro.Initialize(tabIndices, tabValue);
+
+Console.WriteLine(kakuro);
